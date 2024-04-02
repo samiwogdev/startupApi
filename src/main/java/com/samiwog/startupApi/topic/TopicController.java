784e0636817d22a36f6d5fcd480d8d595a34c904
Samuel Ogunbodede <samiwogdev@gmail.com>
2024-04-02 12:26:33 +0100

    Load Topic Service

--- src/main/java/com/samiwog/startupApi/topic/TopicController.java
+++ src/main/java/com/samiwog/startupApi/topic/TopicController.java
@@ -1,10 +1,15 @@
 package com.samiwog.startupApi.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,4 +24,24 @@ public class TopicController {
 		return topicservice.getAllTopics();
 	}
 
+//	@GetMapping("/topics/{foo}")
+//	public Topic getTopic(@PathVariable("foo") String id) {
+//		return topicservice.getTopic(id);
+//	}
+	
+	@GetMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicservice.getTopic(id);
+	}
+	
+//	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	@PostMapping("/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicservice.addTopic(topic);
+	}
+	
+	@PutMapping("/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicservice.updateTopic(topic, id);
+	}
 }
--- src/main/java/com/samiwog/startupApi/topic/TopicService.java
+++ src/main/java/com/samiwog/startupApi/topic/TopicService.java
@@ -1,21 +1,51 @@
 package com.samiwog.startupApi.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
 
-	private List<Topic> topics =  Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(
+			Arrays.asList(
 
-			new Topic("1", "Java", "High Level programming Language"),
-			new Topic("2", "History", "Showcase History"),
-			new Topic("1", "Python", "AI Programming Language")
+					new Topic("java", " Core Java", "Core Java Description"),
+					new Topic("springboot", "Java Framework", "Java Framework Description"),
+					new Topic("javascript", "Javascript", "Javascript Description"))
 			);
-	
-	public List<Topic> getAllTopics(){
+
+	public List<Topic> getAllTopics() {
 		return topics;
+	}
+
+	public Topic getTopic(String id) {
+//		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+
+		/** Alternative approach without using the Lambda expression **/
+		for (Topic topic : topics) {
+	        if (topic.getId().equals(id)) {
+	            return topic;
+	        }
+	    }
+	    throw new NoSuchElementException("Topic with id " + id + " not found");
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+	
+	public void updateTopic(Topic topic, String id) {
+		for(int i = 0; i < topics.size(); i ++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+			
+		}
 	}
 }
